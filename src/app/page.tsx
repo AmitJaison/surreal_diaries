da628ba6ca852105d1093a1908fe15dd6f9b0dd7
AmitJaison <amitjaison31@gmail.com>
2024-12-02 18:13:26 +0530

    Updated Home page and made it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from 'next/link';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const photos = [
   { src: "/home/flower.jpg", alt: "Photo 1" },
@@ -8,18 +10,27 @@ const photos = [
 ];
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="min-h-screen bg-black text-white">
       <header className="p-3 mt-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-black bg-opacity-50 backdrop-blur-md border border-gray-700 rounded-full z-50">
         <div className="container mx-auto flex flex-wrap justify-between items-center">
           <div className="text-2xl font-bold">Surreal Diaries</div>
-          <nav className="flex space-x-6">
+          <nav className={`flex space-x-6 ${isOpen ? 'block' : 'hidden'} sm:flex`}>
             <a href="#" className="hover:text-gray-400">Stories</a>
             <a href="#" className="hover:text-gray-400">Collections</a>
             <Link href="/trips" className="hover:text-gray-400">Trips</Link>
           </nav>
           <button className="bg-white text-black px-7 py-2 rounded-full hover:bg-gray-200 mt-2 sm:mt-0">
             My Story
+          </button>
+          <button className="sm:hidden text-white" onClick={toggleMenu}>
+            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
       </header>
