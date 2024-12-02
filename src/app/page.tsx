081c60b1c8d6e0b08377f7567e6b91a0956ed588
AmitJaison <amitjaison31@gmail.com>
2024-12-02 18:08:13 +0530

    Updated Trips page and made it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from 'next/link';
-// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const photos = [
   { src: "/home/flower.jpg", alt: "Photo 1" },
@@ -11,50 +10,44 @@ const photos = [
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <header className="p-3 mt-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-black bg-opacity-50 backdrop-blur-md border border-gray-700 rounded-full z-50" style={{ borderRadius: '25px' }}>
-        <div className="container mx-auto flex justify-between items-center">
+      <header className="p-3 mt-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-black bg-opacity-50 backdrop-blur-md border border-gray-700 rounded-full z-50">
+        <div className="container mx-auto flex flex-wrap justify-between items-center">
           <div className="text-2xl font-bold">Surreal Diaries</div>
           <nav className="flex space-x-6">
             <a href="#" className="hover:text-gray-400">Stories</a>
             <a href="#" className="hover:text-gray-400">Collections</a>
             <Link href="/trips" className="hover:text-gray-400">Trips</Link>
           </nav>
-          <button className="bg-white text-black px-7 py-2 rounded-full hover:bg-gray-200">
+          <button className="bg-white text-black px-7 py-2 rounded-full hover:bg-gray-200 mt-2 sm:mt-0">
             My Story
           </button>
         </div>
       </header>
-      <main className="flex flex-col justify-center items-center text-center py-20">
+      <main className="flex flex-col justify-center items-center text-center py-20 px-4 sm:px-6 lg:px-8">
         <div className="mb-7 mt-12 pt-12">
           <span className="bg-black-100 px-3 py-1 border border-gray-700 rounded-full z-50">Welcome to my visual diary.</span>
         </div>
-        <h1 className="text-5xl font-bold mb-4">
+        <h1 className="text-3xl sm:text-5xl font-bold mb-4">
           <span className="block bg-gradient-to-r from-gray-300 via-gray-500 to-gray-700 bg-clip-text text-transparent">
-            Unveiling life&apos;s beauty through my personal
+            Unveiling life's beauty through my personal
           </span>
           <span className="block bg-gradient-to-r from-gray-700 via-gray-500 to-gray-300 bg-clip-text text-transparent">
             collection of stories and frames.
           </span>
         </h1>
-        <p className="text-lg mb-8">
-          Feel the stories that each photograph brings to life.
-        </p>
-      </main>
-      <section className="flex flex-col items-center space-y-8 p-8">
-        {photos.map((photo, index) => (
-            <div key={index} className="relative w-full max-w-7xl" style={{ height: 'auto' }}>
-            <Image
-              className="object-cover rounded-lg"
-              src={photo.src}
-              alt={photo.alt}
-              layout="responsive"
-              width={700} // You can adjust the width and height based on your image dimensions
-              height={475}
-              style={{ borderRadius: '25px' }} // Adjust the border radius here
-            />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {photos.map((photo, index) => (
+            <div key={index} className="relative w-full h-64">
+              <Image
+                className="object-cover rounded-lg"
+                src={photo.src}
+                alt={photo.alt}
+                fill
+              />
             </div>
-        ))}
-      </section>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -3,31 +3,31 @@ import Image from "next/image";
 import Link from 'next/link';
 
 const trips = [
-  { src: "/home/trip1.jpg", alt: "Trip 1" },
-  { src: "/home/trip2.jpg", alt: "Trip 2" },
-  { src: "/home/trip3.jpg", alt: "Trip 3" },
+  { src: "/home/flower.jpg", alt: "Trip 1" },
+  { src: "/home/scene.jpg", alt: "Trip 2" },
+  { src: "/home/vala.jpg", alt: "Trip 3" },
 ];
 
 export default function Trips() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <header className="p-3 mt-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-black bg-opacity-50 backdrop-blur-md border border-gray-700 rounded-full z-50" style={{ borderRadius: '25px' }}>
-        <div className="container mx-auto flex justify-between items-center">
+      <header className="p-3 mt-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl bg-black bg-opacity-50 backdrop-blur-md border border-gray-700 rounded-full z-50">
+        <div className="container mx-auto flex flex-wrap justify-between items-center">
           <div className="text-2xl font-bold">Surreal Diaries</div>
           <nav className="flex space-x-6">
             <Link href="/" className="hover:text-gray-400">Home</Link>
             <Link href="/trips" className="hover:text-gray-400">Trips</Link>
           </nav>
-          <button className="bg-white text-black px-7 py-2 rounded-full hover:bg-gray-200">
+          <button className="bg-white text-black px-7 py-2 rounded-full hover:bg-gray-200 mt-2 sm:mt-0">
             My Story
           </button>
         </div>
       </header>
-      <main className="flex flex-col justify-center items-center text-center py-20">
+      <main className="flex flex-col justify-center items-center text-center py-20 px-4 sm:px-6 lg:px-8">
         <div className="mb-7 mt-12 pt-12">
           <span className="bg-black-100 px-3 py-1 border border-gray-700 rounded-full z-50">Welcome to my trips.</span>
         </div>
-        <h1 className="text-5xl font-bold mb-4">
+        <h1 className="text-3xl sm:text-5xl font-bold mb-4">
           <span className="block bg-gradient-to-r from-gray-300 via-gray-500 to-gray-700 bg-clip-text text-transparent">
             Discover my adventures and travels
           </span>
@@ -39,7 +39,7 @@ export default function Trips() {
                 className="object-cover rounded-lg"
                 src={trip.src}
                 alt={trip.alt}
-                layout="fill"
+                fill
               />
             </div>
           ))}
