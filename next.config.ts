315329d903f4bd3c75e1ee0f8afb30038f22fb3f
AmitJaison <amitjaison31@gmail.com>
2024-12-02 23:46:08 +0530

    Configure image domains for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['surreal-diaries.vercel.app'],
+  },
+  // other config options here
 };
 
 export default nextConfig;
