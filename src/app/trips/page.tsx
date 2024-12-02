ee1ac23111a3bcd70ff53db1a3bfeb806e22ae16
AmitJaison <amitjaison31@gmail.com>
2024-12-02 18:31:50 +0530

    Marked page.tsx as a client component and fixed useState error

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -43,7 +43,7 @@ export default function Trips() {
               <h2 className="text-xl font-bold mb-2">{trip.title}</h2>
               <p className="text-gray-400 mb-4">{trip.description}</p>
               <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600">
-                Let's Travel
+                Let&apos;s Travel
               </button>
             </div>
           ))}
