4acf8c94adbfb568c2f258922d1bcd508d8ce00f
AmitJaison <amitjaison31@gmail.com>
2024-12-03 22:08:15 +0530

    Set the header as a component for every page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header/page";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const photos = [
